cd51aacd42ef386dd33f29c07b636801a59c5d88
Carles Amigó <fr3nd@fr3nd.net>
2018-08-14 18:03:25 +0200

    separating dos functions and improving makefile

---
+++ src/include/dos.c
@@ -0,0 +1,301 @@
+// vim:foldmethod=marker:foldlevel=0
+
+#include "dos.h"
+
+
+/*** DOS functions {{{ ***/
+
+char get_screen_size(void) __naked {
+  __asm
+    push ix
+
+    ld a, #0x04
+
+    ld c,IOCTL
+    DOSCALL
+    ld l, e
+
+    pop ix
+    ret
+  __endasm;
+}
+
+int getchar(void) __naked {
+  __asm
+    push ix
+
+    ld c,CONIN
+    DOSCALL
+    ld h, #0x00
+
+    pop ix
+    ret
+  __endasm;
+}
+
+int putchar(int c) __naked {
+  c;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld e,(ix)
+    ld c,CONOUT
+    DOSCALL
+
+    pop ix
+    ret
+  __endasm;
+}
+
+char get_current_drive(void) __naked {
+  __asm
+    push ix
+
+    ld c, CURDRV
+    DOSCALL
+
+    ld h, #0x00
+    ld l, a
+
+    pop ix
+    ret
+  __endasm;
+}
+
+int open(char *fn, char mode) __naked {
+  fn;
+  mode;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld e,0(ix)
+    ld d,1(ix)
+    ld a,2(ix)
+    ld c, OPEN
+    DOSCALL
+
+    cp #0
+    jr z, open_noerror$
+    ld h, #0xff
+    ld l, a
+    jp open_error$
+  open_noerror$:
+    ld h, #0x00
+    ld l, b
+  open_error$:
+    pop ix
+    ret
+  __endasm;
+}
+
+int create(char *fn, char mode, char attributes) __naked {
+  fn;
+  mode;
+  attributes;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld e,0(ix)
+    ld d,1(ix)
+    ld a,2(ix)
+    ld b,3(ix)
+    ld c, CREATE
+    DOSCALL
+
+    cp #0
+    jr z, open_noerror$
+    ld h, #0xff
+    ld l, a
+    jp open_error$
+  create_noerror$:
+    ld h, #0x00
+    ld l, b
+  create_error$:
+    pop ix
+    ret
+  __endasm;
+}
+
+int close(int fp) __naked {
+  fp;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld b,(ix)
+    ld c, CLOSE
+    DOSCALL
+
+    ld h, #0x00
+    ld l,a
+
+    pop ix
+    ret
+  __endasm;
+}
+
+int read(char* buf, unsigned int size, char fp) __naked {
+  buf;
+  size;
+  fp;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld e,0(ix)
+    ld d,1(ix)
+    ld l,2(ix)
+    ld h,3(ix)
+    ld b,4(ix)
+    ld c, READ
+    DOSCALL
+
+    cp #0
+    jr z, read_noerror$
+    ld h, #0xFF
+    ld l, #0xFF
+  read_noerror$:
+    pop ix
+    ret
+  __endasm;
+}
+
+unsigned int write(char* buf, unsigned int size, int fp) __naked {
+  buf;
+  size;
+  fp;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld e,0(ix)
+    ld d,1(ix)
+    ld l,2(ix)
+    ld h,3(ix)
+    ld b,4(ix)
+    ld c, WRITE
+    DOSCALL
+
+    cp #0
+    jr z, write_noerror$
+    ld h, #0xFF
+    ld l, #0xFF
+  write_noerror$:
+    pop ix
+    ret
+  __endasm;
+}
+
+int parse_pathname(char volume_name_flag, char* s) __naked {
+  volume_name_flag;
+  s;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld b,0(ix)
+    ld e,1(ix)
+    ld d,2(ix)
+
+    ld c, PARSE
+    DOSCALL
+
+    pop ix
+    ret
+  __endasm;
+}
+
+void exit(int code) __naked {
+  code;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld b,(ix)
+    ld c, TERM
+    DOSCALL
+
+    pop ix
+    ret
+  __endasm;
+}
+
+char dosver(void) __naked {
+  __asm
+    push ix
+
+    ld c, DOSVER
+    DOSCALL
+
+    ld h, #0x00
+    ld l, b
+
+    pop ix
+    ret
+  __endasm;
+}
+
+void explain(char* buffer, char error_code) __naked {
+  error_code;
+  buffer;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld e,0(ix)
+    ld d,1(ix)
+    ld b,2(ix)
+
+    ld c, EXPLAIN
+    DOSCALL
+
+    ld 0(ix),d
+    ld 1(ix),e
+
+    pop ix
+    ret
+  __endasm;
+}
+
+char get_env(char* name, char* buffer, char buffer_size) __naked {
+  name;
+  buffer;
+  buffer_size;
+  __asm
+    push ix
+    ld ix,#4
+    add ix,sp
+
+    ld l,0(ix)
+    ld h,1(ix)
+    ld e,2(ix)
+    ld d,3(ix)
+    ld b,4(ix)
+
+    ld c, GENV
+    DOSCALL
+
+    ld 0(ix),l
+    ld 1(ix),h
+    ld 2(ix),d
+    ld 3(ix),e
+    ld 4(ix),a
+
+    pop ix
+    ret
+  __endasm;
+}
+
+/*** DOS functions }}} ***/
---
+++ src/include/dos.h
@@ -0,0 +1,44 @@
+// vim:foldmethod=marker:foldlevel=0
+
+#ifndef DOS_H
+#define DOS_H
+
+/* BIOS calls */
+#define EXPTBL #0xFCC1
+
+/* DOS calls */
+#define CONIN   #0x01
+#define CONOUT  #0x02
+#define CURDRV  #0x19
+#define OPEN    #0x43
+#define CREATE  #0x44
+#define CLOSE   #0x45
+#define READ    #0x48
+#define WRITE   #0x49
+#define IOCTL   #0x4B
+#define PARSE   #0x5B
+#define TERM    #0x62
+#define EXPLAIN #0x66
+#define GENV    #0x6B
+#define DOSVER  #0x6F
+
+#define DOSCALL  call 5
+#define BIOSCALL ld iy,(EXPTBL-1)\
+call CALSLT
+
+char get_screen_size(void);
+int getchar(void);
+int putchar(int c);
+char get_current_drive(void);
+int open(char *fn, char mode);
+int create(char *fn, char mode, char attributes);
+int close(int fp);
+int read(char* buf, unsigned int size, char fp);
+unsigned int write(char* buf, unsigned int size, int fp);
+int parse_pathname(char volume_name_flag, char* s);
+void exit(int code);
+char dosver(void);
+void explain(char* buffer, char error_code);
+char get_env(char* name, char* buffer, char buffer_size);
+
+#endif
